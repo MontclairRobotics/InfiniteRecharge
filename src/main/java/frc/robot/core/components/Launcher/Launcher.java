7bf7cee7bd47ae6a44c93af37de45bd9bdb73bc5
Max Knudsen <maxknudsen501@gmail.com>
2020-01-23 15:25:39 -0500

    Fixed a Few Errors in Dylan's Code

--- src/main/java/frc/robot/core/components/Launcher/Launcher.java
+++ src/main/java/frc/robot/core/components/Launcher/Launcher.java
@@ -14,7 +14,7 @@ class Launcher implements LauncherBase{
         motor = null;
         encoder = null;
         transport = null;
-        StateMachineHandler.instantiateState( RestBase(this, null); );
+        StateMachineHandler.instantiateState( new RestBase(this, null) );
         desiredSpeed = 0;
         shooterQueueLength = 0;
 
@@ -25,7 +25,7 @@ class Launcher implements LauncherBase{
         this.motor = motor;
         this.encoder = encoder;
         this.transport = transport;
-        state = new RestBase(this, null);
+        StateMachineHandler.instantiateState( new RestBase(this, null) );
         desiredSpeed = 0;
         shooterQueueLength = 0;
 
