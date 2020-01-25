faead8ac20a926d51c0d4daff066ec8b658d9001
Max Knudsen <maxknudsen501@gmail.com>
2020-01-25 10:19:45 -0500

    Fixed Hardware and Finished BallSuck.java

--- src/main/java/frc/robot/core/components/Transport/BallIntake/BallSuck.java
+++ src/main/java/frc/robot/core/components/Transport/BallIntake/BallSuck.java
@@ -1,12 +1,15 @@
 package frc.robot.core.components.Transport.BallIntake;
 import frc.robot.core.utils.*;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
 import frc.robot.core.Robot;
 import frc.robot.core.components.ControlSystem.AuxillaryButtons;
 
 class BallSuck {
 void ballIntake() {
 if (Robot.controlSystem.getButton(AuxillaryButtons.TEMP)) {
-Hardware.TransportIntake.set
+Hardware.TransportIntake.set(ControlMode.PercentOutput, 0.1);
   }         
  }
 }
