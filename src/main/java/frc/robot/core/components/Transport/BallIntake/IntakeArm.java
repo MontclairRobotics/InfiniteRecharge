e9cc13ea269c0efcb86e995dbdb258c6d4d83ca5
Max Knudsen <maxknudsen501@gmail.com>
2020-01-25 10:56:40 -0500

    Worked on IntakeArm.java (Unfinished)

--- src/main/java/frc/robot/core/components/Transport/BallIntake/IntakeArm.java
+++ src/main/java/frc/robot/core/components/Transport/BallIntake/IntakeArm.java
@@ -1,1 +1,16 @@
-//Fwoop the shute
+package frc.robot.core.components.Transport.BallIntake;
+import frc.robot.core.utils.*;
+import frc.robot.core.Robot;
+import frc.robot.core.components.ControlSystem.AuxillaryButtons;
+
+class IntakeArm {
+    void moveArm () {
+        //TODO
+        //Wait for CAD for Final Design
+    }
+    int armSensor (int ballsStored) {
+        //TODO
+        //Add Sensor for Balls
+        return ballsStored;
+    }
+}
