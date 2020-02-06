5f296b09bc2c855b9fc89ea08bd23e31008372b8
Patrick Henegan <phenegan03@gmail.com>
2020-02-06 18:06:07 -0500

    basic implementation of Transport
    Shooter.java now has a basic implementation of transport (doesn't use states). Some changes had to be made to the Hardware class, fixing some ID classes and making all of the hardware ports, motors, and encoders declared as final

--- src/main/java/frc/robot/core/components/Transport/BallIntake/IntakeArm.java
+++ src/main/java/frc/robot/core/components/Transport/BallIntake/IntakeArm.java
@@ -6,9 +6,9 @@ import frc.robot.core.utils.Hardware;
 
 public class IntakeArm {
     public static void armLower(double speed) {
-        Hardware.IntakeArm.set(ControlMode.PercentOutput, speed);
+        Hardware.IntakeArm.set(speed);
     }
     public void armRaise(double speed) {
-        Hardware.IntakeArm.set(ControlMode.PercentOutput, -speed);
+        Hardware.IntakeArm.set(-speed);
     }
 }
