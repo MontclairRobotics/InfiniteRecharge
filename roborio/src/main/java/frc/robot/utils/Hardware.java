76cc1322a53f48ea16aad473aa453b6bc5f9a47d
Patrick Henegan <phenegan03@gmail.com>
2020-02-24 16:39:19 -0500

    Fixed some errors
    Fixed another error that was occurring by moving the instantiation of objects out of robotInit()

--- roborio/src/main/java/frc/robot/utils/Hardware.java
+++ roborio/src/main/java/frc/robot/utils/Hardware.java
@@ -4,29 +4,18 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Hardware {
-    public static final CANSparkMax DT_FL;
-    public static final CANSparkMax DT_FR;
-    public static final CANSparkMax DT_BL;
-    public static final CANSparkMax DT_BR;
-    public static final CANSparkMax transportBottom;
-    public static final CANSparkMax transportTop;
-    public static final CANSparkMax shooter;
-    public static final CANSparkMax lift;
+    public static final CANSparkMax DT_FL = new CANSparkMax(2, MotorType.kBrushless);
+    public static final CANSparkMax DT_FR = new CANSparkMax(1, MotorType.kBrushless);
+    public static final CANSparkMax DT_BL = new CANSparkMax(9, MotorType.kBrushless);
+    public static final CANSparkMax DT_BR = new CANSparkMax(7, MotorType.kBrushless);
+
+    public static final CANSparkMax transportBottom = new CANSparkMax(3, MotorType.kBrushless); //Not final ID;
+    public static final CANSparkMax transportTop  = new CANSparkMax(5, MotorType.kBrushless);
+    public static final CANSparkMax shooter = new CANSparkMax(6, MotorType.kBrushless);
+
+    public static final CANSparkMax lift = new CANSparkMax(4, MotorType.kBrushless);
     public Hardware() {}
 
     public static void robotInit() {
-
-        //Motor Types are all set to brushless until we know otherwise, some ID's are temporary
-        DT_FL = new CANSparkMax(2, MotorType.kBrushless);
-        DT_BL = new CANSparkMax(1, MotorType.kBrushless);
-        DT_FR = new CANSparkMax(9, MotorType.kBrushless);
-        DT_BR = new CANSparkMax(7, MotorType.kBrushless);
-
-        transportBottom = new CANSparkMax(3, MotorType.kBrushless); //Not final ID
-        transportTop = new CANSparkMax(5, MotorType.kBrushless);
-        
-        shooter = new CANSparkMax(6, MotorType.kBrushless);
-
-        lift = new CANSparkMax(4, MotorType.kBrushless); //Not final ID
     }
 }
