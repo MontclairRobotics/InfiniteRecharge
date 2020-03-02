1ff544cb8ff232d04529c4857a79483e97c26fe5
Patrick Henegan <phenegan03@gmail.com>
2020-03-02 15:32:07 -0500

    Removed unnecessary files
    Someone put project files from the roborio outside of the roborio, except the ones needed are already in the folder

--- roborio/src/main/java/frc/robot/Robot.java
+++ roborio/src/main/java/frc/robot/Robot.java
@@ -7,7 +7,9 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -19,6 +21,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  */
 public class Robot extends TimedRobot{
   private Command m_autonomousCommand;
+  private Joystick test = new Joystick(0);
 
   private RobotContainer m_robotContainer;
 
@@ -58,6 +61,8 @@ public class Robot extends TimedRobot{
 
   @Override
   public void disabledPeriodic() {
+    
+
   }
 
   /**
@@ -96,7 +101,8 @@ public class Robot extends TimedRobot{
    */
   @Override
   public void teleopPeriodic() {
-  }
+    SmartDashboard.putBoolean("A", test.getRawButton(0));
+    }
 
   @Override
   public void testInit() {
