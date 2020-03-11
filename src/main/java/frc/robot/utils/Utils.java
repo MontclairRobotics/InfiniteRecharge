373415c553ae6e527892600fdcc54f90789da5b2
Garrett Burroughs <garrettburroughs02@gmail.com>
2020-03-11 16:38:19 -0400

    Improved motion profiling... needs testing (not to be done near a wall)

--- src/main/java/frc/robot/utils/Utils.java
+++ src/main/java/frc/robot/utils/Utils.java
@@ -7,31 +7,42 @@ import static frc.robot.RobotContainer.driverController;
 
 public class Utils {
 
-    public static boolean threshold(double input, double min, double max){
-        return (input>=min && input<=max);
+    public static boolean threshold(double input, double min, double max) {
+        return (input >= min && input <= max);
     }
 
-    public static void rumbleDriver(boolean rumble){
-        if(rumble){
-            driverController.setRumble(GenericHID.RumbleType.kLeftRumble,1);
-            driverController.setRumble(GenericHID.RumbleType.kRightRumble,1);
-        }else{
-            driverController.setRumble(GenericHID.RumbleType.kLeftRumble,0);
-            driverController.setRumble(GenericHID.RumbleType.kRightRumble,0);
+    public static double constrain(double input, double min, double max) {
+        if (input < min) {
+            return min;
+        }
+        if (input > max) {
+            return max;
+        }
+        return input;
+
+    }
+
+    public static void rumbleDriver(boolean rumble) {
+        if (rumble) {
+            driverController.setRumble(GenericHID.RumbleType.kLeftRumble, 1);
+            driverController.setRumble(GenericHID.RumbleType.kRightRumble, 1);
+        } else {
+            driverController.setRumble(GenericHID.RumbleType.kLeftRumble, 0);
+            driverController.setRumble(GenericHID.RumbleType.kRightRumble, 0);
         }
     }
 
-    public static void rumbleAux(boolean rumble){
-        if(rumble){
-            auxiliaryController.setRumble(GenericHID.RumbleType.kLeftRumble,1);
-            auxiliaryController.setRumble(GenericHID.RumbleType.kRightRumble,1);
-        }else{
-            auxiliaryController.setRumble(GenericHID.RumbleType.kLeftRumble,0);
-            auxiliaryController.setRumble(GenericHID.RumbleType.kRightRumble,0);
+    public static void rumbleAux(boolean rumble) {
+        if (rumble) {
+            auxiliaryController.setRumble(GenericHID.RumbleType.kLeftRumble, 1);
+            auxiliaryController.setRumble(GenericHID.RumbleType.kRightRumble, 1);
+        } else {
+            auxiliaryController.setRumble(GenericHID.RumbleType.kLeftRumble, 0);
+            auxiliaryController.setRumble(GenericHID.RumbleType.kRightRumble, 0);
         }
     }
 
-    public static void rumble(boolean rumble){
+    public static void rumble(boolean rumble) {
         rumbleDriver(rumble);
         rumbleAux(rumble);
     }
